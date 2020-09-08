ce664b1f86bc5284c91090bf6a1897a93353443a
parichrt <69183300+parichrt@users.noreply.github.com>
2020-09-08 12:15:18 +0700

    Add files via upload

---
+++ 3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int operator3(){
+    int a = 5,c;
+
+    c=a;
+    printf("c = %d\n",c);
+    c+=a;
+    printf("c = %d\n",c);
+    c-=a;
+    printf("c = %d\n",c);
+    c*=a;
+    printf("c = %d\n",c);
+    c/=a;
+    printf("c = %d\n",c);
+    c%=a;
+    printf("c = %d\n",c);
+}
---
+++ operator.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int operator1(){
+    int a = 9,b=4,c;
+    c = a+b;
+    printf("a+b = %d + %d = %d \n",a,b,c);
+
+    c= a+b;
+    printf("a-b =%d - %d =%d \n",a,b,c);
+
+    c=a*b;
+    printf("a*b =%d * %d =%d \n",a,b,c);
+
+    c=a/b;
+    printf("a/b =%d / %d =%d \n",a,b,c);
+
+    c=a%b;
+    printf("a%%b =%d %% %d =%d \n",a,b,c);
+
+    return 0;
+}
